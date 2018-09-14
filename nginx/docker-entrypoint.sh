1250e5940881c63284547b08c829de0b012872be
CS-Tao <990296951@qq.com>
2018-09-14 12:49:37 +0800

    修复脚本错误

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -7,8 +7,8 @@ else
     echo "Directory '/var/aqi/logs/nginx' has already existed."
 fi
 
-sed -i "s?AQI_STATIC_FOLDER?/usr/share/nginx/html/${WEB_FOLDER}/${MAIN_WEB_FOLDER}?" /etc/nginx/conf.d/default.conf \
-sed -i "s?MIAN_WEB_FOLDER?/usr/share/nginx/html/${WEB_FOLDER}?" /etc/nginx/conf.d/default.conf \
+sed -i "s?AQI_STATIC_FOLDER?/usr/share/nginx/html/${WEB_FOLDER}/${MAIN_WEB_FOLDER}?" /etc/nginx/conf.d/default.conf
+sed -i "s?MIAN_WEB_FOLDER?/usr/share/nginx/html/${WEB_FOLDER}?" /etc/nginx/conf.d/default.conf
 
 /usr/local/bin/django_check.sh
 /usr/local/bin/start_nginx.sh
